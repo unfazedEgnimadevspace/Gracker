cd12bcf31d3e1299eadaf3f1195d52c5ef2503f2
Akalugo Daniel <akalugidaniel@gmail.com>
2023-05-03 15:13:10 +0100

    Add documentations to the website and app modules

--- app/controllers/website/pages_controller.rb
+++ app/controllers/website/pages_controller.rb
@@ -1,3 +1,5 @@
+#This pages controller serve static pages of the website that require no prior authentication 
+# It renders the home page, about page, contact and pricing pages...
 class Website::PagesController < ApplicationController
   
   def home
