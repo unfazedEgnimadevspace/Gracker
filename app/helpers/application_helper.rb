87c1675c84e77a7383142a046eedca731a660aec
Akalugo Daniel <akalugidaniel@gmail.com>
2023-06-13 08:34:27 +0100

    Fixed the broken gifs for development only, production has been left as a todo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,20 @@
 module ApplicationHelper
-    def full_title(page_title = '')
-        base_title = "Gracker"
-        if page_title.empty?
-            base_title
-        else
-            page_title + ' | ' + base_title
-        end
+  def full_title(page_title = '')
+    base_title = "Gracker"
+    if page_title.empty?
+        base_title
+    else
+        page_title + ' | ' + base_title
     end
+  end
+
+  def exercise_gif_url(exercise)
+    formatted_id = sprintf("%04d", exercise.id)
+    if Rails.env.development?
+      image_path("/exercise_gifs/#{formatted_id}.gif")
+    else  
+      # TODO: Logic for creating url in 
+    end
+  end
     
 end
