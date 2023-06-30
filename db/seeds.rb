0995ce51cf19464875538950b279b9f3bda226bc
Akalugo Daniel <96634477+unfazedEgnimadevspace@users.noreply.github.com>
2023-06-30 04:56:28 +0100

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,5 @@
-puts " === Database seeding starting === "
-
-require 'json'
-data_hash = JSON.load_file("./db/exercises.json")
-puts "Creating exercises"
-
-data_hash.each do |exercise_object|
-    print "."
-    Exercise.create!( 
-        name: exercise_object["name"],
-        body_part: exercise_object["bodyPart"], 
-        equipment: exercise_object["equipment"], 
-        gif_url: exercise_object["gifUrl"], 
-        id: exercise_object["id"],
-        target: exercise_object["target"]
-    )
-end
-
-
-puts " === Database seeding ending === "
+JSON
+  .load_file("./db/exercises.json")
+  .map { _1.transform_keys(&:underscore) }
+  .tap { Exercise.upsert_all(_1) }
+  .then { puts "Loaded exercises: #{_1.size}" }
