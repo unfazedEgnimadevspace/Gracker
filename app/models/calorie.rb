9b753765806a91ad3aa22496bc610d7b8b328e76
Akalugo Daniel <akalugidaniel@gmail.com>
2023-07-11 14:51:08 +0100

    Changed the calorie's model name to food

--- app/models/calorie.rb
+++
@@ -1,2 +0,0 @@
-class Calorie < ApplicationRecord
-end
--- db/migrate/20230711134905_create_foods.rb
+++ db/migrate/20230711134905_create_foods.rb
@@ -1,8 +1,8 @@
-class CreateCalories < ActiveRecord::Migration[7.0]
+class CreateFoods < ActiveRecord::Migration[7.0]
   def change
-    create_table :calories do |t|
+    create_table :foods do |t|
       t.integer :user_id
-      t.string :food_name
+      t.string :name
       t.string :meal_type
       t.datetime :consumed_at
       t.text :notes
