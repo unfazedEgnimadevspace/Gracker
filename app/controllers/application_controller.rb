b43db63addc743d21acb72412552b81f3d27bf31
Daniel Akalugo <96634477+unfazedEgnimadevspace@users.noreply.github.com>
2023-07-15 04:55:08 +0100

    Updated application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
     protected 
   
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password, :password_confirmation])
     end
+    def after_sign_in_path_for(resource)
+      app_root_url
+    end
+    
 end
