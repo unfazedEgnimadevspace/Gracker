7f6b1a425215269fd99d23c1ab849fe905cd3f41
Akalugo Daniel <akalugidaniel@gmail.com>
2023-07-19 19:03:45 +0100

    Implemented chartkick and groupdate libraries used for displaying data in graph format

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import "flowbite";
+import "chartkick"
+import "Chart.bundle"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "flowbite", to: "https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.turbo.min.js"
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
