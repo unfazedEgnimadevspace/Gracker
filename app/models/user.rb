b1d5895e9b89473c2548bcc04d363d3e411a0553
Akalugo Daniel <akalugidaniel@gmail.com>
2023-07-28 11:17:06 +0100

    Created a one to many relationship between user and planned workouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :foods      
-  has_many :trainings 
+  has_many :foods, dependent: :destroy      
+  has_many :trainings, dependent: :destroy
+  has_many :planned_workouts, dependent: :destroy 
 end
